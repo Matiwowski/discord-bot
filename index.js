a5733a8d51d5a08b4b0fe40b7b939b7900aada71
Matiwowski <matiwitkowski311@gmail.com>
2024-08-03 20:39:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,29 +17,64 @@ const token = process.env.BOT_TOKEN;
 
 const diseases = {
     "Grypa": {
-        mandatorySymptoms: ["Gorączka", "Kaszel", "Ból głowy"],
-        optionalSymptoms: ["Zmęczenie", "Ból mięśni", "Nudności"],
-        testResults: ["Powiększone węzły chłonne", "Podwyższone ciśnienie", "Gorączka"]
+        mandatorySymptoms: ["Gorączka", "Suchy kaszel", "Ból głowy w okolicach czoła"],
+        optionalSymptoms: ["Zmęczenie", "Ból mięśni", "Nudności", "Biegunka", "Wymioty"],
+        testResults: ["Powiększone węzły chłonne", "Podwyższone ciśnienie", "Gorączka", "Szybkie testy wirusowe w kierunku gypy są pozytywne (RIDT: +)", "RT-PCR wskazuje na występowanie materiału genetycznego wirusa grypy"]
     },
-    "Covid-19": {
-        mandatorySymptoms: ["Gorączka", "Kaszel", "Duszność"],
-        optionalSymptoms: ["Ból gardła", "Utrata smaku", "Utrata węchu"],
-        testResults: ["Powiększone węzły chłonne", "Podwyższone ciśnienie", "Gorączka", "Zmniejszona saturacja tlenu"]
+"Zapalenie przełyku": {
+        mandatorySymptoms: ["Zgaga", "Problemy z połykaniem pokarmu", "Ból w trakcie połykania"],
+        optionalSymptoms: ["Gorączka", "Ból mięśni", "Ból głowy", "Zamostkowy, piekący ból w klatce piersiowej"],
+        testResults: ["Bioptat (pobrany kawałek) błony śluzowej przełyku mocno zaczerwieniony, z objawami włóknienia", "W badaniu endoskopowym widoczne zmiany krwotoczne przełyku"]
     },
-    "Przeziębienie": {
-        mandatorySymptoms: ["Katar", "Ból gardła"],
-        optionalSymptoms: ["Kaszel", "Ból głowy", "Zmęczenie"],
-        testResults: ["Lekko podwyższone ciśnienie", "Niewielka gorączka"]
+"Napad kamicy nerkowej (Kolka nerkowa)": {
+        mandatorySymptoms: ["Ból w okolicy lędźwiowej", "Silne parcie na mocz", "Wydalanie małej ilości moczu na każdą mikcję"],
+        optionalSymptoms: ["Nudności", "Wymioty"],
+        testResults: ["Krwinkomocz (Obecność zwiększonej ilości krwinek w wydalanym moczu)", "USG uwidacznia złóg i poszerzenie dróg moczowych", "Obecność w moczu kryształów szczawianu wapnia"]
     },
-    "Zapalenie oskrzeli": {
-        mandatorySymptoms: ["Kaszel", "Ból w klatce piersiowej"],
-        optionalSymptoms: ["Gorączka", "Duszność", "Zmęczenie"],
-        testResults: ["Podwyższone ciśnienie", "Powiększone węzły chłonne", "Gorączka"]
+"Ostre zapalenie trzustki (OZT)": {
+        mandatorySymptoms: ["Nagły, bardzo silny ból brzucha (w nadbrzuszu)", "Nudności", "Wymioty nieprzynoszące ulgi", "Gorączka", "Osłabienie"],
+        optionalSymptoms: ["Ból brzucha promieniuje do kręgosłupa", "Ból w okolicy pępka"],
+        testResults: ["Gorączka", "Osłabienie lub zniesienie szmerów perystaltycznych jelit", "Wzmożone napięcie powłok brzusznych", "Tachykardia (przyśpieszona akcja serca)", "Zwiękosza aktywność enzymów trzustkowych (>=3x norma)", "Zwiększone stęzenie CRP", "Zwiększone stężenie kreatyniny w surowicy", "USG: powiększenie i zatarcie granic trzustki", "Zwiększona aktywność lipazy w surowicy"]
     },
-    "Angina": {
-        mandatorySymptoms: ["Ból gardła", "Gorączka"],
-        optionalSymptoms: ["Ból głowy", "Ból mięśni", "Trudności w przełykaniu"],
-        testResults: ["Powiększone węzły chłonne", "Gorączka", "Obrzęk gardła"]
+"Niepowikłane zapalenie pęcherza moczowego": {
+        mandatorySymptoms: ["Ból podczas oddawania moczu", "Częste oddawanie moczu", "Ból brzucha (w okolicy nadłonowej)"],
+        optionalSymptoms: ["Tkliwość okolicy nadłonowej", "Lekkie zaczerwienienie moczu"],
+        testResults: ["Leukocyturia (Obecność zwiększonej ilości leukocytów w moczu)", "Krwiomocz (Obecność krwi w moczu)", "Bakteriomocz (Obecność bakterii w moczu)"]
+    },
+"Niepowikłane ostre odmiedniczkowe zapalenie nerek (OOZN)": {
+        mandatorySymptoms: ["Ból brzucha (w lewym, górnym kwadracie)", "Dreszcze", "Złe samopoczucie", "Gorączka"],
+        optionalSymptoms: ["Tkliwość okolicy nadłonowej", "Ból podczas oddawania moczu"],
+        testResults: ["Gorączka", "Leukocyturia (Obecność zwiększonej ilości leukocytów w moczu)", "Bakteriomocz (Występowanie białka w moczu)"]
+    },
+"Ostre zapalenie uchyłków": {
+        mandatorySymptoms: ["Ból brzucha (lewego dolnego kwadratu brzucha i podbrzusza)", "Gorączka", "Biegunka"],
+        optionalSymptoms: ["Zmiana rytmu wypróżnień"],
+        testResults: ["Podczas badania fizykalnego wyczuwalna obrona mięśniowa w okolicach podbrzusza", "Gorączka", "TK jamy brzusznej i miednicy mniejszej wykazuje: pogrubienie ścian okrężnicy i naciek zapalny w tkance tłuszczowej"]
+    },
+"Zapalenie błony śluzowej żołądka wywołane przez Helicobacter pylori": {
+        mandatorySymptoms: ["Rozlany ból brzucha"],
+        optionalSymptoms: ["Pieczenie w nadbrzuszu", "Wczesna sytość"],
+        testResults: ["Wykrycie H. pylori w kale lub wycinku endoskopowym"]
+    },
+"Ostre zapalenie błony śluzowej gardła i migdałków (angina) wywołane paciorkowcem PBHA": {
+        mandatorySymptoms: ["Nagły, silny ból gardła", "Ból podczas połykania", "Ból głowy", "Nudności", "Gorączka"],
+        optionalSymptoms: ["Wymioty"],
+        testResults: ["Powiększone węzły chłonne", "Gorączka", "Widoczne zapalenie błony śluzowej gardła (kolor żywoczerwony)", "Na migdałkach widoczny ograniczony wysięk", "Tzw. szyki test na antygen PBHA: wynik dodatni (+)", "Posiew wymazu z gardła i migdałków wskazuje na zakażenie PBHA [+]"]
+    },
+"Pozaszpitalne zapalenie płuc (PZP)": {
+        mandatorySymptoms: ["Ból w klatce piersiowej", "Kaszel", "Gorączka"],
+        optionalSymptoms: ["Dreszcze", "Zimne poty", "Ból mięśni"],
+        testResults: ["Gorączka", "Tachykardia (przyśpieszona akcja serca)", "Obniżona saturacja (SpO2 < 95%)", "Podczas osłuchiwania klatki piersiowej stwierdza się: trzeszczenia i/lub szmer oskrzelowy", "Stężenie CRP podwyższone (> 20mg/l)", "RTG klatki piersiowej w projekcji tylno przedniej (PA) i bocznej uwidaczniają: zacieniania miąższowe płuc", "TK uwidacznia zmiany w miąższu płuc o charakterze zapalnym", "USG: naciek w miąższu płuc", "posiew plwociny i krwi w kierunku MRSA: ujemy [-]"; "popłuczyny z oskrzeli i płyn z płukania oskrzelowo-pęcherzykowego wskazuje na zakażenie bakteryjne"]
+    },
+"Zakażenie norowirusami": {
+        mandatorySymptoms: ["Wodnista biegunka", "Nudności", "Ból brzucha", "Pogorszone samopoczucie"],
+        optionalSymptoms: ["Wymioty", "Ból mięśni"],
+        testResults: ["RT-qCPR pobranego od pacjetna materiału wykazuje zakażenie norowirusami"]
+    },
+"Trądzik bez hiperandrogenizmu": {
+        mandatorySymptoms: ["Krosty na twarzy"],
+        optionalSymptoms: ["Wągry (ciemne zmiany na skórze) w okolicach twarzy", "Białe zaskórniki na twarzy", "Białe zaskórniki na plecach", "Cysty podskórne w okolicach podbródka", "Krosty na plecach"],
+        testResults: ["Pobranie wysięku ze zmian skórnych wskazuje na kolonizację przez droboustroje", "Brak znaczących zmian w badaniu aktywności hormonów we krwi"]
     }
 };
 
